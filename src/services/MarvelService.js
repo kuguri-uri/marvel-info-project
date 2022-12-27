5f7c64f2fe9b10c35ae81013e0c288cc8c9926ab
Vera <mokeeva.vk@phystech.edu>
2022-12-27 16:41:21 +0300

    get character(s)

---
+++ src/services/MarvelService.js
@@ -0,0 +1,25 @@
+class MarvelService {
+
+    _apiBase = 'https://gateway.marvel.com:443/v1/public/';
+    _apiKey = 'apikey=82418d30bdc8b6f6505d25cc5e53ca80';
+
+    getResource = async (url) => {
+        let res = await fetch(url);
+
+        if (!res.ok) {
+            throw new Error(`Could not fetch ${url}, status: ${res.status}`);
+        }
+
+        return await res.json();
+    }
+
+    getAllCharacters = () => {
+        return this.getResource(`${this._apiBase}characters?limit=9&offset=210&${this._apiKey}`);
+    }
+
+    getCharacter = (id) => {
+        return this.getResource(`${this._apiBase}characters/${id}?${this._apiKey}`);
+    }
+}
+
+export default MarvelService;
