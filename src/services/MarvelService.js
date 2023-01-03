51eb98053393e1099b1bd95cd37efaa4efbf4461
Vera <mokeeva.vk@phystech.edu>
2023-01-03 15:15:55 +0300

    ErrorBoundary

--- src/services/MarvelService.js
+++ src/services/MarvelService.js
@@ -2,6 +2,7 @@ class MarvelService {
 
     _apiBase = 'https://gateway.marvel.com:443/v1/public/';
     _apiKey = 'apikey=82418d30bdc8b6f6505d25cc5e53ca80';
+    _baseOffset = 210;
 
     getResource = async (url) => {
         let res = await fetch(url);
@@ -14,7 +15,7 @@ class MarvelService {
     }
 
     getAllCharacters = async () => {
-        const res = await this.getResource(`${this._apiBase}characters?limit=9&offset=210&${this._apiKey}`);
+        const res = await this.getResource(`${this._apiBase}characters?limit=9&offset=${this._baseOffset}&${this._apiKey}`);
         return res.data.results.map(this._transformChracter);
     }
 
