6f63daa4ba10c91a1cd6ceb925a922b177282b36
Vera <mokeeva.vk@phystech.edu>
2023-01-08 22:41:08 +0300

    refs

--- src/components/charList/CharList.js
+++ src/components/charList/CharList.js
@@ -1,12 +1,10 @@
-import { Component } from 'react';
-import PropTypes from 'prop-types'
+import {Component} from 'react';
+import PropTypes from 'prop-types';
 
 import Spinner from '../spinner/Spinner';
 import ErrorMessage from '../errorMessage/ErrorMessage';
 import MarvelService from '../../services/MarvelService';
-
 import './charList.scss';
-// import abyss from '../../resources/img/abyss.jpg';
 
 class CharList extends Component {
 
@@ -18,14 +16,14 @@ class CharList extends Component {
         offset: 210,
         charEnded: false
     }
-
+    
     marvelService = new MarvelService();
 
     componentDidMount() {
-        this.onReqest();
+        this.onRequest();
     }
 
-    onReqest = (offset) => {
+    onRequest = (offset) => {
         this.onCharListLoading();
         this.marvelService.getAllCharacters(offset)
             .then(this.onCharListLoaded)
@@ -45,7 +43,7 @@ class CharList extends Component {
         }
 
         this.setState(({offset, charList}) => ({
-            charList: [...charList, ...newCharList], 
+            charList: [...charList, ...newCharList],
             loading: false,
             newItemLoading: false,
             offset: offset + 9,
@@ -53,21 +51,36 @@ class CharList extends Component {
         }))
     }
 
-    onCharLoading = () => {
-        this.setState({
-            loading: true
-        })
-    }
-
     onError = () => {
         this.setState({
-            loading: false,
-            error: true
+            error: true,
+            loading: false
         })
     }
 
+    itemRefs = [];
+
+    setRef = (ref) => {
+        this.itemRefs.push(ref);
+    }
+
+    focusOnItem = (id) => {
+        // Я реализовал вариант чуть сложнее, и с классом и с фокусом
+        // Но в теории можно оставить только фокус, и его в стилях использовать вместо класса
+        // На самом деле, решение с css-классом можно сделать, вынеся персонажа
+        // в отдельный компонент. Но кода будет больше, появится новое состояние
+        // и не факт, что мы выиграем по оптимизации за счет бОльшего кол-ва элементов
+
+        // По возможности, не злоупотребляйте рефами, только в крайних случаях
+        this.itemRefs.forEach(item => item.classList.remove('char__item_selected'));
+        this.itemRefs[id].classList.add('char__item_selected');
+        this.itemRefs[id].focus();
+    }
+
+    // Этот метод создан для оптимизации, 
+    // чтобы не помещать такую конструкцию в метод render
     renderItems(arr) {
-        const items =  arr.map((item) => {
+        const items =  arr.map((item, i) => {
             let imgStyle = {'objectFit' : 'cover'};
             if (item.thumbnail === 'http://i.annihil.us/u/prod/marvel/i/mg/b/40/image_not_available.jpg') {
                 imgStyle = {'objectFit' : 'unset'};
@@ -76,14 +89,25 @@ class CharList extends Component {
             return (
                 <li 
                     className="char__item"
+                    tabIndex={0}
+                    ref={this.setRef}
                     key={item.id}
-                    onClick={() => this.props.onCharSelected(item.id)}>
+                    onClick={() => {
+                        this.props.onCharSelected(item.id);
+                        this.focusOnItem(i);
+                    }}
+                    onKeyPress={(e) => {
+                        if (e.key === ' ' || e.key === "Enter") {
+                            this.props.onCharSelected(item.id);
+                            this.focusOnItem(i);
+                        }
+                    }}>
                         <img src={item.thumbnail} alt={item.name} style={imgStyle}/>
                         <div className="char__name">{item.name}</div>
                 </li>
             )
         });
-        
+        // А эта конструкция вынесена для центровки спиннера/ошибки
         return (
             <ul className="char__grid">
                 {items}
@@ -91,12 +115,12 @@ class CharList extends Component {
         )
     }
 
-    render () {
+    render() {
 
         const {charList, loading, error, offset, newItemLoading, charEnded} = this.state;
-
-        const items = this.renderItems(charList);
         
+        const items = this.renderItems(charList);
+
         const errorMessage = error ? <ErrorMessage/> : null;
         const spinner = loading ? <Spinner/> : null;
         const content = !(loading || error) ? items : null;
@@ -106,12 +130,11 @@ class CharList extends Component {
                 {errorMessage}
                 {spinner}
                 {content}
-    
                 <button 
                     className="button button__main button__long"
                     disabled={newItemLoading}
                     style={{'display': charEnded ? 'none' : 'block'}}
-                    onClick={() => this.onReqest(offset)}>
+                    onClick={() => this.onRequest(offset)}>
                     <div className="inner">load more</div>
                 </button>
             </div>
