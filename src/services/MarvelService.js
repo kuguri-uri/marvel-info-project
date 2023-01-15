50abd3779d284cf4a842912c2eaafe4347fd701c
Vera <mokeeva.vk@phystech.edu>
2023-01-15 19:42:31 +0300

    custom hooks

--- src/services/MarvelService.js
+++ src/services/MarvelService.js
@@ -2,7 +2,7 @@ import { useHttp } from "../hooks/http.hook";
 
 const useMarvelService = () => {
 
-    const {loading, request, error} = useHttp();
+    const {loading, request, error, clearError} = useHttp();
 
     const _apiBase = 'https://gateway.marvel.com:443/v1/public/';
     const _apiKey = 'apikey=82418d30bdc8b6f6505d25cc5e53ca80';
@@ -38,7 +38,7 @@ const useMarvelService = () => {
         }
     }
 
-    return {loading, error, getAllCharacters, getCharacter}
+    return {loading, error,clearError, getAllCharacters, getCharacter}
 }
 
 export default useMarvelService;
