23d7468c2e683a9049da189f3be8170354e39075
Vera <mokeeva.vk@phystech.edu>
2023-01-15 17:41:41 +0300

    own hooks started

---
+++ src/hooks/http.hook.js
@@ -0,0 +1,25 @@
+import { useState, useCallback } from "react";
+
+export const useHttp = () => {
+    const [loading, setLoading] = useState(false);
+    const [error, setError] = useState(null);
+
+    const request = useCallback(async (url, method = 'GET', body = null, headers = {'Content-Type': 'application/json'}) => {
+
+        setLoading(true);
+
+        try {
+            const response = await fetch(url, {method, body, headers});
+
+            if (!response.ok) {
+                throw new Error(`Could not fetch ${url}, status: ${res.status}`);
+            }
+
+            const data = await response.json();
+
+
+        } catch(e) {
+
+        }
+    }, [])
+}
