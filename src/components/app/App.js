237a41088a7e198fc8a870919e1345e2ecf136c4
Vera <mokeeva.vk@phystech.edu>
2023-02-06 14:39:21 +0300

    CharList animated

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,6 +1,7 @@
 import {lazy, Suspense} from 'react';
 import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
 
+
 import AppHeader from "../appHeader/AppHeader";
 import Spinner from '../spinner/Spinner';
 
