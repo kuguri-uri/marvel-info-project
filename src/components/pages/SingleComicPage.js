d98635c4b34b3c823c31566101cc0de7bf0cfc77
Vera <mokeeva.vk@phystech.edu>
2023-02-13 15:40:26 +0300

    with react-helmet

--- src/components/pages/SingleComicPage.js
+++ src/components/pages/SingleComicPage.js
@@ -1,5 +1,6 @@
 import { useParams, Link } from 'react-router-dom';
 import { useState, useEffect } from 'react';
+import { Helmet } from 'react-helmet';
 
 import useMarvelService from '../../services/MarvelService';
 import Spinner from '../spinner/Spinner';
@@ -46,7 +47,15 @@ const View = ({comic}) => {
     const {title, description, pageCount, thumbnail, language, price} = comic;
 
     return (
+        
         <div className="single-comic">
+            <Helmet>
+                <meta
+                    name="description"
+                    content={`${title} comics book`}
+                    />
+                <title>{title}</title>
+            </Helmet>
             <img src={thumbnail} alt={title} className="single-comic__img"/>
             <div className="single-comic__info">
                 <h2 className="single-comic__name">{title}</h2>
